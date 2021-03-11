e03e3c8be6067467f3c24758551f5639e843585c
Sqm <Sqm@DESKTOP-FLTSPGL>
2021-03-11 15:52:56 -0600

    Green Light |

--- Tiles2048/test/createTest.py
+++ Tiles2048/test/createTest.py
@@ -7,7 +7,7 @@ class CreateTest(unittest.TestCase):
 #  useful.  Please delete and replace with your own test code.
 
 #checks to make sure the size of the string is correct
-    def test_create_SadPathTest010(self):
+ """   def test_create_SadPathTest010(self):
         userParms = {'grid': '0000000000000000'}
         actualResult = create._create(userParms)
         self.assertEqual(actualResult, userParms)
@@ -16,5 +16,14 @@ class CreateTest(unittest.TestCase):
         userParms = {'grid': '0020022000000000'}
         actualResult = create._create(userParms)
         self.assertEqual(actualResult['grid'].count('2'), userParms['grid'].count('2'))
+"""        
+    def test_create_HappyPathTest030(self):
+        userParms = {'grid': '0020020000000000'}
+        actualResult = create._create(userParms)
+        self.assertEqual(actualResult['grid'].count('2'), userParms['grid'].count('2'))
         
+    def test_create_HappyPathTest040(self):
+        numOfTwos = 2
+        actualResult = create._create(None)
+        self.assertEqual(numOfTwos, actualResult['grid'].count('2'))
         
