8dc295bf008964bb1da22d9e7d0bd78ceeb22db7
Sqm <Sqm@DESKTOP-FLTSPGL>
2021-03-11 16:15:46 -0600

    Green Light |

--- Tiles2048/create.py
+++ Tiles2048/create.py
@@ -2,6 +2,12 @@ import random
 
 def _create(userParms):
     grid = '0200000000000020'
+    locOne = random.randint(0, 15)
+    locTwo = random.randint(0, 15)
+    while locTwo == locOne:
+        locTwo = random.randint(0,15)
+    grid[locOne] = '2'
+    grid[locTwo] = '2'
     
     result = {'grid': grid}
     return result
