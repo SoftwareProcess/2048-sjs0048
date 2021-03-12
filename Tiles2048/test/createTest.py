7d1d15c824a5e01a8bbe0bd1bf0737fc30b3939a
Sqm <Sqm@DESKTOP-FLTSPGL>
2021-03-11 18:00:13 -0600

    Green Light |

--- Tiles2048/test/createTest.py
+++ Tiles2048/test/createTest.py
@@ -21,13 +21,15 @@ class CreateTest(unittest.TestCase):
     def test_create_HappyPathTest030(self):
         userParms = {'grid': '0020020000000000'}
         actualResult = create._create(userParms)
-        print(actualResult['grid'])
         self.assertEqual(actualResult['grid'].count('2'), userParms['grid'].count('2'))
         
     
     def test_create_HappyPathTest040(self):
         numOfTwos = 2
         actualResult = create._create(None)
-        print(actualResult['grid'])
         self.assertEqual(numOfTwos, actualResult['grid'].count('2'))
         
+    def test_create_HappyPathTest050(self):
+        sizeOfGrid = 16
+        actualResult = create._create(None)
+        self.assertEqual(sizeOfGrid, len(actualResult['grid']))
