ce15aac6fb7e4c7625346362360f225267420992
Sqm <Sqm@DESKTOP-FLTSPGL>
2021-04-02 14:58:57 -0500

    Red Light |

--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -10,13 +10,13 @@ class ShiftTest(unittest.TestCase):
         userParms = create._create(None)
         userParms['grid'] = '000020020000000'
         actualResult = shift._shift(userParms)
-        self.assertEqual(actualResult, "Error: This grid is too small")
+        self.assertEqual(actualResult, "Error: This grid is too small.")
         
     def test_create_ProperGridSize2(self):
         userParms = create._create(None)
         userParms['grid'] = '00000000000000000000000000000000000000000000000000000000000000000000000000000000'
         actualResult = shift._shift(userParms)
-        self.assertEqual(actualResult, "Error: This grid is too large")
+        self.assertEqual(actualResult, "Error: This grid is too large.")
 
     
     def test_create_StringIntoList(self):
