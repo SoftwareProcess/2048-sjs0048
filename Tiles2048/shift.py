afbe8ba23d658626822592b0e24c55af32443b00
Sqm <Sqm@DESKTOP-FLTSPGL>
2021-04-02 14:58:04 -0500

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -2,7 +2,7 @@ def _shift(userParms):
     if len(userParms['grid']) < 16:
         result = "Error: This grid is too small."
         return result
-    elif len(userParms['grid'] > 64)
+    elif len(userParms['grid'] > 64):
         result = "Error: This grid is too large."
         return result
     
