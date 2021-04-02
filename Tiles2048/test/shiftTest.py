2b0e4dcd10c86c7c5b5b60946d8bbf15592bc76a
Sqm <Sqm@DESKTOP-FLTSPGL>
2021-04-02 16:46:50 -0500

    Red Light |

--- Tiles2048/test/shiftTest.py
+++ Tiles2048/test/shiftTest.py
@@ -6,27 +6,28 @@ import Tiles2048.shift as shift
 class ShiftTest(unittest.TestCase):
     
     
-    def test_create_GridTooSmall(self):
+    def test_shift_GridTooSmall(self):
         userParms = create._create(None)
         userParms['grid'] = '000020020000000'
         actualResult = shift._shift(userParms)
         self.assertEqual(actualResult, "Error: This grid is too small. Please check the input string.")
-        
-    def test_create_GridTooLarge(self):
-        userParms = create._create(None)
-        userParms['grid'] = '00000000000000000'
-        actualResult = shift._shift(userParms)
-        self.assertEqual(actualResult, "Error: This grid is too large. Please check the input string.")
 
-    def test_create_GridTooLarge2(self):
+    def test_shift_GridTooLarge(self):
         userParms = create._create(None)
         userParms['grid'] = '10241024102410241024102410241024102410241024102410241024102410240'
         actualResult = shift._shift(userParms)
         self.assertEqual(actualResult, "Error: This grid is too large. Please check the input string.")
     
-    def test_create_StringIntoList(self):
+    def test_shift_StringIntoList(self):
         userParms = create._create(None)
         userParms['grid'] = '1024128048163225651264210242566480'
         actualResult = shift._shift(userParms)
         comparison = [1024, 128, 0, 4, 8, 16, 32, 256, 512, 64, 2, 1024, 256, 64, 8, 0]
-        self.assertEqual(actualResult, comparison)  
+        self.assertEqual(actualResult, comparison)
+        
+    def test_shift_ListInto2dList(self):
+        userParms = create._create(None)
+        userParms['grid'] = '1024128048163225651264210242566480'
+        actualResult = shift._shift(userParms)
+        comparison = [[1024, 128, 0, 4], [8, 16, 32, 256], [512, 64, 2, 1024], [256, 64, 8, 0]]
+        self.assertEqual(actualResult, comparison)
