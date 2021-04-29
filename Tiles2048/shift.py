757a5909ae8a49d7b12bc48260d7e39c9bec5de4
Sqm <Sqm@DESKTOP-FLTSPGL>
2021-04-28 19:15:17 -0500

    Red Light |

--- Tiles2048/shift.py
+++ Tiles2048/shift.py
@@ -20,11 +20,11 @@ def _shift(userParms):
         output['status'] = 'error: missing grid'
         return output
     
-    if (userParms['direction'].lower() != 'up' and userParms['direction'].lower() != 'down'
+    """if (userParms['direction'].lower() != 'up' and userParms['direction'].lower() != 'down'
           and userParms['direction'].lower() != 'right' and userParms['direction'].lower() != 'left'
           and userParms['direction'] != ''):
         output['status'] = 'error: invalid direction'
-        return output
+        return output"""
     
     userEncoded = (str(userParms['grid']) + '.' + str(userParms['score'])).encode()
     if userParms['integrity'] != hashlib.sha256(userEncoded).hexdigest().upper():
